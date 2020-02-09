0423e3870b40d6574fdd4ef0f54c7125515d99b0
tmig <tmig@yourlabs.org>
2020-02-09 11:31:20 +0100

    test conf

--- drf_nested_multipart/__init__.py
+++ drf_nested_multipart/__init__.py
@@ -1,1 +1,9 @@
+from rest_framework import settings
+
+
+settings.DEFAULTS['TEST_REQUEST_RENDERER_CLASSES'].append(
+    'drf_nested_multipart.renderers.NestedMultiPartRenderer'
+)
+
+
 __version__ = '0.1.0'
